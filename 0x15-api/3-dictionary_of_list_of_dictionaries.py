8fe4902dca34685dcb6725e39e9935f8de0afde3
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-10-25 11:40:17 -0700

    update

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,32 +8,37 @@ import sys
 
 if __name__ == "__main__":
 
-    emp_id = sys.argv[1]
-
+    emp_id = 1
     base_url = f'https://jsonplaceholder.typicode.com/'
 
-    users = f'users?id={emp_id}'
-    todos = f'todos?userId={emp_id}'
-    done = f'{todos}&completed=true'
-    not_done = f'{todos}&completed=false'
+    dict_data = {}
 
-    user_data = requests.get(f'{base_url}{users}').json()
-    name = user_data[0].get("name")
-    username = user_data[0].get("username")
+    while True:
 
-    todo_data = requests.get(f'{base_url}{todos}').json()
-    done_data = requests.get(f'{base_url}{done}').json()
+        users = f'users?id={emp_id}'
+        todos = f'todos?userId={emp_id}'
+        done = f'{todos}&completed=true'
+        not_done = f'{todos}&completed=false'
 
-    len_todo = len(todo_data)
-    len_done = len(done_data)
+        user_data = requests.get(f'{base_url}{users}').json()
 
-    with open(f'todo_all_employees.json', 'w') as dict_file:
-        data = [
+        if not user_data:
+            break
+
+        name = user_data[0].get("name")
+        username = user_data[0].get("username")
+
+        todo_data = requests.get(f'{base_url}{todos}').json()
+
+        dict_data[emp_id] = [
                 {
                     "username": username,
                     "task": task.get("title"),
                     "completed": task.get("completed")
                 }
                 for task in todo_data
-                ]
-        json.dump(data, dict_file)
+            ]
+        emp_id += 1
+
+    with open(f'todo_all_employees.json', 'w') as dict_file:
+        json.dump(dict_data, dict_file)
